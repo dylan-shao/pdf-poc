da473a4ac3588207a7a1d4debf17f69c9d581fa7
Dylan-Shao <yangshao2014@gmail.com>
2018-02-09 23:13:02 -0500

    Part1:ui, add App1 component, with form and two buttons

---
+++ client/src/App1.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import './App1.css';
+
+class App1 extends Component {
+  constructor() {
+    super();
+    this.state = {
+      address: ''
+    };
+  }
+
+  onChange = e => {
+    this.setState({
+      address: e.target.value
+    })
+  }
+  render() {
+    const { address } = this.state;
+    return (
+      <div className="App">
+        <h1>Part 1</h1>
+        <form className="form address-form">
+          <label htmlFor="address">Address </label>
+          <input 
+            type="text" 
+            name="address"
+            id="address" 
+            value={address} 
+            onChange={this.onChange} />
+          <button type="submit">Submit 1</button>
+          <button>Submit 2</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default App1;
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App1 from './App1';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<App1 />, document.getElementById('root'));
