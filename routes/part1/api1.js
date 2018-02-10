8e85916ed18b644d27b7ab3645b12cf4ad1a76a5
Dylan-Shao <yangshao2014@gmail.com>
2018-02-09 22:57:45 -0500

    Part1: generate pdf using pdfkit

--- routes/part1/api1.js
+++ routes/part1/api1.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const part1Router = express.Router();
+const PDFDocument = require('pdfkit');
 
-part1Router.get('/test', (req, res, next)=>{
-  res.send('test server');
+part1Router.get('/pdf', (req, res, next)=>{
+  let filename = 'myPdf';
+  filename = encodeURIComponent(filename) + '.pdf';
+
+  res.setHeader('Content-disposition', 'inline; filename="' + filename + '"');
+  res.setHeader('Content-type', 'application/pdf');
+
+  doc = new PDFDocument();
+
+  doc.text('pdf content');
+
+  doc.pipe(res);
+
+  doc.end();
 });
 
 module.exports = part1Router;
